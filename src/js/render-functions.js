6c4730d7e31fe3fa62c14b22d64b2c63e2b8b5d2
Yevhenii Sarancha <yevhenii.sarancha@outlook.com>
2024-04-13 18:10:32 +0300

    css redesign

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,28 +1,39 @@
 export function createGallaryMarkup(array) {
+  function capitalizeFirstLetter(tag) {
+    return tag.charAt(0).toUpperCase() + tag.slice(1);
+  }
+
   return array
     .map(
       image =>
         `<li class="gallery-item">
-          <a class="gallery-link" href="${image.largeImageURL}">
-            <img class="gallery-image" src="${image.webformatURL}" alt="${image.tags}" data-source="${image.largeImageURL}">
-          </a>
-          <div>
+          <div class="image-container">
+            <a class="gallery-link" href="${image.largeImageURL}">
+              <img class="gallery-image" src="${image.webformatURL}" alt="${
+          image.tags
+        }" data-source="${image.largeImageURL}">
+            </a>
+          </div>
+          <div class="gallery-tags">
+            <h2 class="gallery-tags-header">${capitalizeFirstLetter(image.tags)}</h2>
+          </div>
+          <div class="desc-container">
             <ul class="list-data">
-                <li>
-                    <h3>Likes</h3>
-                    <p>${image.likes}</p>
+                <li class="desc-item">
+                    <h3 class="gallery-header">Likes</h3>
+                    <p class="gallery-text">${image.likes}</p>
                 </li>
-                <li>
-                    <h3>Views</h3>
-                    <p>${image.views}</p>
+                <li class="desc-item">
+                    <h3 class="gallery-header">Views</h3>
+                    <p class="gallery-text">${image.views}</p>
                 </li>
-                <li>
-                    <h3>Comments</h3>
-                    <p>${image.comments}</p>
+                <li class="desc-item">
+                    <h3 class="gallery-header">Comments</h3>
+                    <p class="gallery-text">${image.comments}</p>
                 </li>
-                <li>
-                    <h3>Downloads</h3>
-                    <p>${image.downloads}</p>
+                <li class="desc-item">
+                    <h3 class="gallery-header">Downloads</h3>
+                    <p class="gallery-text">${image.downloads}</p>
                 </li>
             </ul>
           </div>
