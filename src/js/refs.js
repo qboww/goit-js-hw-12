75c8d7333f70c14a39409b6a9fe3e0206423fffd
Yevhenii Sarancha <yevhenii.sarancha@outlook.com>
2024-04-13 18:26:16 +0300

    minor changes

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,3 +2,4 @@ export const list = document.querySelector('.js-gallery');
 export const form = document.querySelector('.js-form');
 export const loader = document.querySelector('.js-loader');
 export const input = document.querySelector('.js-search-input');
+export const load = document.querySelector('.js-button-load');
